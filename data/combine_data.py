33273148df1e283a7b69657d61acdddcb045baf2
Majo <m.bednar@student.rug.nl>
2024-05-03 13:55:02 +0200

    combined data

---
+++ data/combine_data.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import glob
+import os
+from sklearn.utils import shuffle
+
+language = input('Language: ')
+
+if language != 'all':
+    # List all CSV files in the directory
+    file_paths = glob.glob('datasets_original_splits/' + language + '/*.csv')
+else:
+    file_paths = glob.glob('datasets_original_splits/*/*.csv')
+
+# Initialize an empty list to store data from all files
+combined_data = []
+
+# Read data from each file and append to the combined_data list
+for file_path in file_paths:
+    data = pd.read_csv(file_path)
+    combined_data.append(data)
+
+# Concatenate data from all files into a single dataframe
+combined_df = pd.concat(combined_data, ignore_index=True)
+
+# Shuffle the combined dataframe
+combined_df_shuffled = shuffle(combined_df)
+
+# Write the shuffled dataframe to a new CSV file
+directory = 'datasets_custom_splits/' + language + '/'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+combined_df_shuffled.to_csv(directory + language + '_combined.csv', index=False)
