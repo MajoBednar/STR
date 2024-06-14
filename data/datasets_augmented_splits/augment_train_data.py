2aefd613dd179b3b51b45cfedfb69ebb68b2653b
Majo <m.bednar@student.rug.nl>
2024-06-14 14:46:13 +0200

    augmentation script + afr augmented

--- data/datasets_augmented_splits/augment_train_data.py
+++ data/datasets_augmented_splits/augment_train_data.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import nltk
 import random
-nltk.download('punkt')
+from sklearn.utils import shuffle
 
 """This script augments a (low-resource) training set with sentences from a chosen file.
-Each pair consist of two equivalent sentences with a relatedness score of 1.
+Each new pair consist of two equivalent sentences with a relatedness score of 1.
 The number of new pairs is equal to the number of pairs in the original training set."""
+
+try:
+    # try to use the punkt tokenizer
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    # if not found, download the punkt resource
+    nltk.download('punkt')
 
 # choose the language: afr/esp/hin/mar/pan, and the file for new sentences
 language = input('Language: ')
@@ -14,18 +21,45 @@ file = directory + input('Name of the file with new sentences: ')
 
 # read the original training set (and remove ids)
 original_training_set = pd.read_csv(directory + language + '_train.csv')
-original_training_set = original_training_set.drop(columns=['PairID'])
 
 # get the number of samples in the training set to be used for the number of new samples
 num_samples = len(original_training_set)
 
-# open the file with new sentences
+# find the shortest and longest sentence from the original training set (to limit the length of usable sentences)
+sentence_lengths = []
+for pair in original_training_set['Text']:
+    sentence1, sentence2 = pair.split('\n')
+    sentence_lengths.append(len(nltk.word_tokenize(sentence1)))
+    sentence_lengths.append(len(nltk.word_tokenize(sentence2)))
+min_words, max_words = min(sentence_lengths), max(sentence_lengths)
+
+# open the file with new sentences and filter the ones that fall in the range of appropriate length
 with open(file, encoding='utf-8') as f:
-    # get sentences from 10 to 20 words
     usable_sentences = []
     for line in f:
-        print(line)
+        line = line.strip()
+        if 10 <= len(nltk.word_tokenize(line)) <= 20:
+            usable_sentences.append(line)
 
+# select the appropriate number of sentences from usable sentences
+selected_sentences = random.sample(usable_sentences, num_samples)
 
-print(num_samples)
-print(original_training_set)
+# duplicate the sentences and separate them with newline
+new_sentence_pairs = [sentence + '\n' + sentence for sentence in selected_sentences]
+
+# create a new DataFrame from the selected sentences with a score of 1
+new_data = {
+    'PairID': [f'{language.upper()}-aug-{i}' for i in range(len(new_sentence_pairs))],
+    'Text': new_sentence_pairs,
+    'Score': [1] * len(new_sentence_pairs)
+}
+new_df = pd.DataFrame(new_data)
+
+# concatenate the new DataFrame with the original DataFrame
+combined_df = pd.concat([original_training_set, new_df], ignore_index=True)
+
+# shuffle the new training set
+combined_df = shuffle(combined_df)
+
+# replace the new training set
+combined_df.to_csv(directory + language + '_train.csv', index=False)
